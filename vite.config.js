714b9cfd229f035be481c215215dcb4de70d69c5
root <root@hosting-hawk.sscdev.software>
2025-08-19 12:55:31 +0000

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
     host: "0.0.0.0",
     allowedHosts: [
       "1cfe43f0-6383-4729-818a-8fa28b4a1a24-00-czpaeac4au4g.riker.replit.dev",
+      "sellaro.id",
+      "www.sellaro.id",
     ],
   },
 });
